bc567f84b191d510bc54233fea73ce6970bfcb2c
Luke Burns <lbburns13@gmail.com>
2018-09-13 16:56:22 +0100

    Update footer.php

--- php/footer.php
+++ php/footer.php
@@ -4,7 +4,7 @@
         <div class="col text-center"><a href="#">Back to top</a></div>
     </div>
     <div class="row">
-        <div class="col-sm-4">&copy; 2016-2018<br>The Online Centre for Religious Studies (OCRS)<br><a href="/privacy">Privacy policy</a><br><a href="/terms">Terms and conditions</a></div>
+        <div class="col-sm-4">&copy; 2016-2018<br>The Online Centre for Religious Studies (OCRS)<br><a href="/about">About us</a><br><a href="/privacy">Privacy policy</a><br><a href="/terms">Terms and conditions</a></div>
 
         <div class="col-sm-4">
             <div class="row">
